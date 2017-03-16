3b1d7af2ff6b08bb696ffb69e9356ceffb065411
Vijay Jadhav <vijayjadhav008@gmail.com>
2017-03-16 20:59:31 +0530

    ok all

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 // server.js
 const express = require('express');
 const app = express();
+
+
+
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
